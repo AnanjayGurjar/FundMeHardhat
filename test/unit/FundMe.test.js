7dbe3bdde7a53ec53ea7461a5bb1788879c99c2a
AnanjayGurjar <ananjaygurjar@outlook.com>
2022-09-11 16:09:22 +0530

    unit tests for fund function

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,7 +1,9 @@
 const { deployments, ethers, getNamedAccounts } = require("hardhat");
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 describe("FundMe", async () => {
     let fundMe, deployer, mockV3Aggregator;
+    // const sendValue = "1000000000000000000";    //1 eth
+    const sendValue = ethers.utils.parseEther("1");
     beforeEach(async () => {
         // const accounts = await ethers.getSigners();
         // const accountZero = accounts[0];
@@ -21,6 +23,24 @@ describe("FundMe", async () => {
         it("set the aggregator address correctly", async () => {
             const response = await fundMe.priceFeed();
             assert.equal(response, mockV3Aggregator.address);
+        });
+    });
+
+    describe("fund", async () => {
+        it("Fails if you don't send enough ETH", async () => {
+            await expect(fundMe.fund()).to.be.revertedWith(
+                "You need to spend more ETH!"
+            );
+        });
+        it("Updates the amound funded data structure", async () => {
+            await fundMe.fund({ value: sendValue });
+            const response = await fundMe.addressToAmountFunded(deployer);
+            assert.equal(response.toString(), sendValue.toString());
+        });
+        it("Adds funder to array of funders", async () => {
+            await fundMe.fund({ value: sendValue });
+            const funder = await fundMe.funders(0);
+            assert.equal(funder, deployer);
         });
     });
 });
